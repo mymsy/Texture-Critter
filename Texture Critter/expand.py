af87bb51ecc5f76a97acbf0fd6e8454b24440537
mymsy <redglasses+github@gmail.com>
2015-03-01 16:00:47 -0600

    Changed options parsing to use argparse

--- Texture Critter/expand.py
+++ Texture Critter/expand.py
@@ -6,22 +6,27 @@ Created on Feb 3, 2015
 
 from PIL import Image
 import sys
+import argparse
 
 if __name__ == '__main__':
     # Minimum arguments - input (source) and output (expanded) filenames
-    if len(sys.argv) < 3:
-        print "Usage:", sys.argv[0], "input output"
-
-    infile = sys.argv[1]
-    outfile = sys.argv[2]
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="the source texture file")
+    parser.add_argument("output_file", help="the destination file")
+    
     # TODO gonna need to read some options
+        # targeted synthesis w/ target file (default off)
+        # gaussian weighting (default flat)
+        # neighbourhood size (default ?)
+        # randomisation method (default none)
+
+    args = parser.parse_args()
 
     # Read the source image
     try:
-        source_image = Image.open(infile)
+        source_image = Image.open(args.input_file)
     except IOError:
-        print "Could not open input file", infile
+        print "Could not open input file", args.input_file
         exit(1)
     
     # This is where stuff will happen
@@ -29,9 +34,9 @@ if __name__ == '__main__':
     
     # Write the final image
     try:
-        expansion.save(outfile)
+        expansion.save(args.output_file)
     except IOError:
-        print "Could not write output file", outfile
+        print "Could not write output file", args.output_file
         exit(1)
         
     exit(0)
